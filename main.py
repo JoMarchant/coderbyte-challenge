2a7905cde98c081a5f41a1607c2729021bc9ad22
JoMarchant <jose_marchant@uc.cl>
2022-08-13 21:03:46 -0400

    Agrega función top10_users_with_most_tweets

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import data_handler
 import tweets_functions
 
-def main():
-  tweets = data_handler.read_data('farmers-protest-tweets-2021-03-5.json')
 
-  top10 = tweets_functions.top10_retweeted(tweets)
+def main():
+    # tweets = data_handler.read_data('farmers-protest-tweets-2021-03-5.json')
+    tweets = data_handler.read_data_limited('farmers-protest-tweets-2021-03-5.json', 100)
+
+    top10_retweeted = tweets_functions.top10_retweeted(tweets)
+    top10_users_with_most_tweets = tweets_functions.top10_users_with_most_tweets(tweets)
+
+
 
 if __name__ == '__main__':
-  main()
+    main()
